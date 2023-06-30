f02562ccd8c1bf8709b66ab1a12dde325e7daaf6
David Marchand <david.marchand@redhat.com>
2023-06-30 11:38:51 +0200

    net/ice: use mbuf free bulk helper
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- drivers/net/ice/ice_rxtx_vec_avx512.c
+++ drivers/net/ice/ice_rxtx_vec_avx512.c
@@ -865,8 +865,7 @@ ice_tx_free_bufs_avx512(struct ice_tx_queue *txq)
 	struct ice_vec_tx_entry *txep;
 	uint32_t n;
 	uint32_t i;
-	int nb_free = 0;
-	struct rte_mbuf *m, *free[ICE_TX_MAX_FREE_BUF_SZ];
+	struct rte_mbuf *free[ICE_TX_MAX_FREE_BUF_SZ];
 
 	/* check DD bits on threshold descriptor */
 	if ((txq->tx_ring[txq->tx_next_dd].cmd_type_offset_bsz &
@@ -930,32 +929,9 @@ ice_tx_free_bufs_avx512(struct ice_tx_queue *txq)
 	}
 
 normal:
-	m = rte_pktmbuf_prefree_seg(txep[0].mbuf);
-	if (likely(m)) {
-		free[0] = m;
-		nb_free = 1;
-		for (i = 1; i < n; i++) {
-			m = rte_pktmbuf_prefree_seg(txep[i].mbuf);
-			if (likely(m)) {
-				if (likely(m->pool == free[0]->pool)) {
-					free[nb_free++] = m;
-				} else {
-					rte_mempool_put_bulk(free[0]->pool,
-							     (void *)free,
-							     nb_free);
-					free[0] = m;
-					nb_free = 1;
-				}
-			}
-		}
-		rte_mempool_put_bulk(free[0]->pool, (void **)free, nb_free);
-	} else {
-		for (i = 1; i < n; i++) {
-			m = rte_pktmbuf_prefree_seg(txep[i].mbuf);
-			if (m)
-				rte_mempool_put(m->pool, m);
-		}
-	}
+	for (i = 0; i < n; i++)
+		free[i] = txep[i].mbuf;
+	rte_pktmbuf_free_bulk(free, n);
 
 done:
 	/* buffers were freed, update counters */
