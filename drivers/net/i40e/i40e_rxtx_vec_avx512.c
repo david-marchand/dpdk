4a69e80b8136561aadda50120c468c7436fcec7f
David Marchand <david.marchand@redhat.com>
2023-06-30 11:38:51 +0200

    net/i40e: use mbuf free bulk helper
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- drivers/net/i40e/i40e_rxtx_vec_avx512.c
+++ drivers/net/i40e/i40e_rxtx_vec_avx512.c
@@ -760,8 +760,7 @@ i40e_tx_free_bufs_avx512(struct i40e_tx_queue *txq)
 	struct i40e_vec_tx_entry *txep;
 	uint32_t n;
 	uint32_t i;
-	int nb_free = 0;
-	struct rte_mbuf *m, *free[RTE_I40E_TX_MAX_FREE_BUF_SZ];
+	struct rte_mbuf *free[RTE_I40E_TX_MAX_FREE_BUF_SZ];
 
 	/* check DD bits on threshold descriptor */
 	if ((txq->tx_ring[txq->tx_next_dd].cmd_type_offset_bsz &
@@ -821,33 +820,11 @@ i40e_tx_free_bufs_avx512(struct i40e_tx_queue *txq)
 		goto done;
 	}
 
-	m = rte_pktmbuf_prefree_seg(txep[0].mbuf);
-	if (likely(m)) {
-		free[0] = m;
-		nb_free = 1;
-		for (i = 1; i < n; i++) {
-			rte_prefetch0(&txep[i + 3].mbuf->cacheline1);
-			m = rte_pktmbuf_prefree_seg(txep[i].mbuf);
-			if (likely(m)) {
-				if (likely(m->pool == free[0]->pool)) {
-					free[nb_free++] = m;
-				} else {
-					rte_mempool_put_bulk(free[0]->pool,
-							     (void *)free,
-							     nb_free);
-					free[0] = m;
-					nb_free = 1;
-				}
-			}
-		}
-		rte_mempool_put_bulk(free[0]->pool, (void **)free, nb_free);
-	} else {
-		for (i = 1; i < n; i++) {
-			m = rte_pktmbuf_prefree_seg(txep[i].mbuf);
-			if (m)
-				rte_mempool_put(m->pool, m);
-		}
+	for (i = 0; i < n; i++) {
+		rte_prefetch0(&txep[i + 3].mbuf->cacheline1);
+		free[i] = txep[i].mbuf;
 	}
+	rte_pktmbuf_free_bulk(free, n);
 
 done:
 	/* buffers were freed, update counters */
