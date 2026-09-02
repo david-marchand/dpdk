8c7c14193c190eabe84c34f1976a14d895ba1ad8
David Marchand <david.marchand@redhat.com>
2026-09-02 17:44:29 +0200

    eal/freebsd: fix a FD leak in the alarm subsystem
    Caught by code review, while looking at rte_intr_instance_free() users.
    
    Even if the FD is not used, it must still be closed when uninitialising
    the alarm subsystem.
    
    Fixes: 26021a715067 ("eal/bsd: support alarm API")
    Cc: stable@dpdk.org
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- lib/eal/freebsd/eal_alarm.c
+++ lib/eal/freebsd/eal_alarm.c
@@ -54,6 +54,8 @@ rte_eal_alarm_cleanup(void)
 	int ret = rte_intr_callback_unregister_sync(intr_handle,
 			eal_alarm_callback, (void *)-1);
 	if (ret >= 0) {
+		close(rte_intr_fd_get(intr_handle));
+		rte_intr_fd_set(intr_handle, -1);
 		rte_intr_instance_free(intr_handle);
 		intr_handle = NULL;
 	}
@@ -62,7 +64,7 @@ rte_eal_alarm_cleanup(void)
 int
 rte_eal_alarm_init(void)
 {
-	int fd;
+	int fd = -1;
 
 	intr_handle = rte_intr_instance_alloc(RTE_INTR_INSTANCE_F_PRIVATE);
 	if (intr_handle == NULL) {
@@ -88,6 +90,8 @@ rte_eal_alarm_init(void)
 
 	return 0;
 error:
+	if (fd >= 0)
+		close(fd);
 	rte_intr_instance_free(intr_handle);
 	return -1;
 }
